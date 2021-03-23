6a93323a5f6eeb7dc36537166b0684715ea9478a
Eric Mok <ericmok@cargofe.com>
2021-03-23 23:21:47 +0800

    init

---
+++ src/OKAY.Property.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OKAY.Property.MVC.MvcApplication" Language="C#" %>
---
+++ src/OKAY.Property.MVC/Models/Properties.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OKAY.Property.MVC.Models
+{
+    public class Property
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [MaxLength(200)]
+        [Required]
+        public string name { get; set; }
+        public int bedroom { get; set; }
+        [Required]
+        public bool isAvailable { get; set; }
+        [Required]
+        public decimal leasePrice { get; set; }
+        [Required]
+        [MaxLength(128)]
+        public string userId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+        public virtual ICollection<Transaction> Transactions { get; set; }
+    }
+}
